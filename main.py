00c41f4f76236bf33b4689e24bbdd233951a879b
I4104 <86468607+I4104@users.noreply.github.com>
2021-06-26 09:43:10 +0700

    Update main.py

--- main.py
+++ main.py
@@ -115,7 +115,7 @@ async def on_message(message):
         elif (meaning == "Prepositions"): meaning = "Pre"
             
         embed = discord.Embed(color=0xff0000)
-        embed.add_field(message.author.mention() + " " + name=message.content.split(" ")[1] + '('+ meaning  +'): ' + translate_text, value=contents, inline=False)
+        embed.add_field(message.author.mention + " " + name=message.content.split(" ")[1] + '('+ meaning  +'): ' + translate_text, value=contents, inline=False)
         embed.set_footer(text='Bot version: 1.0.2 - Admin: Tạ Đăng Khoa')
         await message.channel.send(embed=embed)
 
