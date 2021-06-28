f7e456b2301d25e8750d5d906fb950a47d86ed74
I4104 <86468607+I4104@users.noreply.github.com>
2021-06-28 12:01:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -242,7 +242,8 @@ async def on_message(message):
 
         with open('vocabulary.txt', 'r') as reader:
             for line in reader:
-                all_words.append(line)
+                if (line != "\n" && line != "\r" && line != "\r\n" && line != "\n\r"):
+                    all_words.append(line)
 
         words = list()
         while len(words) < int(message.content.split(" ")[1]):
